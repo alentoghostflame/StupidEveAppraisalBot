af0e00d330edf8cee2d921898930a20928725033
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2020-07-21 12:51:12 -0500

    Rewrote the surface level code to work with discord.py subcommands, better help menus, better text
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- self_roles_module/self_roles.py
+++ self_roles_module/self_roles.py
@@ -1,9 +1,12 @@
-from self_roles_module.self_role_control import self_role_control
+from self_roles_module import role_cmds
 from self_roles_module import text
 from self_roles_module.storage import RoleSelfAssignData
 from alento_bot import DiscordBot, StorageManager
 from discord.ext import commands
 import logging
+from typing import Optional
+import discord
+import discord.errors
 
 
 logger = logging.getLogger("main_bot")
@@ -24,18 +27,55 @@ class SelfRoleCog(commands.Cog, name="Self Roles"):
     def __init__(self, storage: StorageManager):
         self.storage: StorageManager = storage
 
-    @commands.command(name="self_role", aliases=["role", ])
-    async def self_role_command(self, context: commands.Context, arg1=None, arg2=None, arg3=None):
-        if context.guild:
-            guild_data: RoleSelfAssignData = self.storage.guilds.get(context.guild.id, "self_roles_data")
-            await self_role_control(guild_data, context, arg1, arg2, arg3)
-        else:
-            await context.send(text.SELF_ROLE_CONTROL_NO_GUILD)
+    @commands.guild_only()
+    @commands.bot_has_guild_permissions(manage_roles=True)
+    @commands.group(name="role", invoke_without_command=True, brief=text.ROLE_GROUP_BRIEF)
+    async def role_group(self, context: commands.Context, role_keyword: Optional[str]):
+        if context.invoked_subcommand is None:
+            if role_keyword:
+                guild_data: RoleSelfAssignData = self.storage.guilds.get(context.guild.id, "self_roles_data")
+                if role_keyword.lower() in guild_data.roles:
+                    await role_cmds.toggle_user_role(guild_data, context, role_keyword)
+                else:
+                    await context.send(text.ROLE_INVALID_SUBCOMMAND)
+            else:
+                await role_cmds.send_help_embed(context)
 
-    @self_role_command.error
-    async def self_role_err(self, context: commands.Context, error: Exception):
-        if isinstance(error, commands.MissingPermissions):
-            await context.send(text.MISSING_ADMIN_PERMISSION)
+    @commands.guild_only()
+    @role_group.command(name="info", brief=text.ROLE_INFO_BRIEF)
+    async def role_info(self, context: commands.Context, role_keyword: Optional[str]):
+        guild_data: RoleSelfAssignData = self.storage.guilds.get(context.guild.id, "self_roles_data")
+        await role_cmds.send_list_embed(guild_data, context, role_keyword)
+
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    @role_group.command(name="add", brief=text.ROLE_ADD_BRIEF)
+    async def role_add(self, context: commands.Context, role_keyword: Optional[str],
+                       role_mention: Optional[discord.Role]):
+        guild_data: RoleSelfAssignData = self.storage.guilds.get(context.guild.id, "self_roles_data")
+        await role_cmds.add_role(guild_data, context, role_keyword, role_mention)
+
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    @role_group.command(name="remove", aliases=["rm", ], brief=text.ROLE_REMOVE_BRIEF)
+    async def role_remove(self, context: commands.Context, role_keyword: Optional[str]):
+        guild_data: RoleSelfAssignData = self.storage.guilds.get(context.guild.id, "self_roles_data")
+        await role_cmds.remove_self_role(guild_data, context, role_keyword)
+
+    @role_group.error
+    @role_info.error
+    @role_add.error
+    @role_remove.error
+    async def role_error_handler(self, context: commands.Context, error: Exception):
+        if isinstance(error, commands.NoPrivateMessage):
+            await context.send(text.NO_PRIVATE_MESSAGE)
+        elif isinstance(error, commands.BotMissingPermissions):
+            await context.send(text.BOT_MISSING_MANAGE_ROLES_PERMISSION)
+        elif isinstance(error, commands.CommandInvokeError) and isinstance(error.original, discord.errors.Forbidden):
+            await context.send(text.BOT_CANT_ADD_OR_REMOVE_ROLE)
+        elif isinstance(error, commands.MissingPermissions):
+            await context.send(text.USER_MISSING_ADMIN_PERMISSION)
         else:
-            await context.send(f"ERROR OCCURED: {type(error)}, {error}\nREPORT THIS TO SOMBRA/ALENTO GHOSTFLAME!")
+            await context.send(f"A CRITICAL ERROR OCCURED:\n\n {type(error)}\n\n {error}\n\n"
+                               f"REPORT THIS TO SOMBRA/ALENTO GHOSTFLAME!")
             raise error
--- self_roles_module/text.py
+++ self_roles_module/text.py
@@ -1,17 +1,34 @@
-MISSING_ADMIN_PERMISSION = "You are missing the permission(s) required to do that."
-ROLE_DOESNT_EXIST = "Role not found."
+BOT_MISSING_MANAGE_ROLES_PERMISSION = "I require the \"Manage Roles\" permission to assign roles to users."
+BOT_CANT_ADD_OR_REMOVE_ROLE = "I can't add/remove that role, is the role hierarchy properly set up?"
+USER_MISSING_ADMIN_PERMISSION = "This command requires the Administrator permission, something that you lack."
+ROLE_INVALID_SUBCOMMAND = "Subcommand or role keyword not found. To see available subcommands, do `role`"
+NO_PRIVATE_MESSAGE = "This command can only be used on a server, not in private messages."
+ROLE_KEYWORD_NOT_FOUND = "Role keyword `{}` not found."
+ROLE_NO_LONGER_IN_GUILD = "The role corresponding to that keyword no longer exists, contact a server administrator " \
+                          "to have it removed."
 
-SELF_ROLE_CONTROL_MISSING_ARG_1 = "`role role_keyword|list|add|remove|rm` Used to self-assign a role or manage which " \
-                                  "roles can be self assigned."
-SELF_ROLE_CONTROL_NO_GUILD = "This command can only be run in a server, not a DM."
-SELF_ROLE_CONTROL_LIST_WRONG_ARG = "`role list role_keyword` Lists all users who have the role."
-SELF_ROLE_CONTROL_TOGGLE_ROLE_DOESNT_EXIST = "That role doesn't exist anymore, contact an administrator?"
-SELF_ROLE_CONTROL_TOGGLE_ADDED = "Role added."
-SELF_ROLE_CONTROL_TOGGLE_REMOVED = "Role removed."
-SELF_ROLE_CONTROL_ADD_MISSING_ARGS = "`role add role_keyword @role` adds a role to add when someone uses the " \
-                                     "role_keyword"
-SELF_ROLE_CONTROL_ADD_SUCCESS = "Added role keyword `{}`"
-SELF_ROLE_CONTROL_REMOVE_MISSING_ARG = "`role remove|rm role_keyword` removes a role from being added when someone " \
-                                       "uses the role_keyword."
-SELF_ROLE_CONTROL_REMOVE_NOT_FOUND = "`{}` doesn't exist."
-SELF_ROLE_CONTROL_REMOVE_SUCCESS = "Role keyword `{}` deleted."
+
+ROLE_GROUP_BRIEF = "Allows users to self-assign roles securely."
+ROLE_INFO_BRIEF = "Shows keywords and the roles they are assigned to."
+ROLE_ADD_BRIEF = "Assigns a keyword with a role for users to use."
+ROLE_REMOVE_BRIEF = "Removes a keyword from being used."
+
+
+ROLE_HELP_DESCRIPTION = "Allows users to give themselves roles without giving them risky permissions. Do `role` to " \
+                        "view this embed."
+ROLE_HELP_USAGE = "`role role_keyword`: Gives the user the role the keyword is assigned to."
+ROLE_HELP_EDITING = f"`role add`: {ROLE_ADD_BRIEF}\n`role remove`: {ROLE_REMOVE_BRIEF}\n Both of these commands " \
+                    f"require the Discord \"Administrator\" permission."
+ROLE_HELP_INFORMATION = f"`role info`: {ROLE_INFO_BRIEF}\n`role info role_keyword` show all the people who has the " \
+                        f"role corresponding to that keyword."
+
+
+ROLE_TOGGLE_REMOVED = "Role removed from your account."
+ROLE_TOGGLE_ADDED = "Role added to your account."
+
+ROLE_ADD_MISSING_ARGS = "`role add role_keyword @role` Do `role` for more information."
+ROLE_ADD_SUCCESS = "Role keyword `{}` added, now assigned to role `{}`."
+
+ROLE_REMOVE_MISSING_ARGS = "`role remove role_keyword` or `role rm role_keyword` Do `role` for more information."
+ROLE_REMOVE_SUCCESS = "Role keyword `{}` removed."
+
